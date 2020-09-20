356d9eac295d0602c374f1f0772d82f0d076ea12
Rochisha <rochisha.agarwal2302@gmail.com>
2020-09-20 17:08:58 +0530

    Updates

---
+++ precache-manifest.73cbf8f37bfb890c6a65caad4bc02f53.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b4998750603ef6cc3e73b46d5b00d2",
+    "url": "/rochisha0.github.io/index.html"
+  },
+  {
+    "revision": "349ee934f7811800d432",
+    "url": "/rochisha0.github.io/static/css/main.0bcbe63c.chunk.css"
+  },
+  {
+    "revision": "8df8112e69b59de09749",
+    "url": "/rochisha0.github.io/static/js/2.ae648cbb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rochisha0.github.io/static/js/2.ae648cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349ee934f7811800d432",
+    "url": "/rochisha0.github.io/static/js/main.80be8c6c.chunk.js"
+  },
+  {
+    "revision": "07509536e3e8a253ed2a",
+    "url": "/rochisha0.github.io/static/js/runtime-main.c978a000.js"
+  },
+  {
+    "revision": "949d7bd021f6cc695943a90024abf286",
+    "url": "/rochisha0.github.io/static/media/books.949d7bd0.jpg"
+  },
+  {
+    "revision": "dabd50e62fe3630926a108ce40d89411",
+    "url": "/rochisha0.github.io/static/media/finance.dabd50e6.jpeg"
+  },
+  {
+    "revision": "9e6229411ca7f72b17e909d40127f8e5",
+    "url": "/rochisha0.github.io/static/media/my-pic.9e622941.jpg"
+  },
+  {
+    "revision": "f87b8b075388da0c2b2335a61ce488f7",
+    "url": "/rochisha0.github.io/static/media/plotly.f87b8b07.png"
+  }
+]);
